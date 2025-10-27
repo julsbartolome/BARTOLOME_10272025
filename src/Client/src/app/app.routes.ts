72125a9093fe246951abb88f82778a015febd615
julsbartolome <julius-kenneth.bartolome@hill-labs.co.nz>
2025-10-27 15:27:27 +0800

    Add Upload and home components. Add routing. Removed test and configure schematics for component and service.

--- src/Client/src/app/app.routes.ts
+++ src/Client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../features/home/home.component';
+import { UploadComponent } from '../features/upload/upload.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{path:'', component:HomeComponent},
+	{path:'upload', component:UploadComponent},
+	{path: '**', redirectTo:''}
+];
